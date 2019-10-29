9c2186a3b3b4d3012dd6387fa6a81f2ca4ddb6ad
Morpheus Hsieh <morpheus.hsieh@gmail.com>
2019-10-29 16:22:03 +0800

    commit

--- src/main/MonkeyJumpCrossRiver/Solution.java
+++ src/main/MonkeyJumpCrossRiver/Solution.java
@@ -2,15 +2,11 @@ package main.MonkeyJumpCrossRiver;
 
 import java.util.Arrays;
 
-//import java.util.HashSet;
-//import java.util.Set;
-
 public class Solution 
 {
   public int solution(int[] A, int D) 
   {
     int N = A.length;
-//    System.out.println("\r\n"+D+", "+Arrays.toString(A));
     
     boolean stones[] = new boolean[N+2];
     Arrays.fill(stones, false);
@@ -22,10 +18,8 @@ public class Solution
       for (int j=0; j<N; j++) {
         if (A[j] == i) { stones[j+1] = true; }
       }
-//      System.out.println(i+": "+Arrays.toString(stones));
 
       boolean isCrossRiver = checkCrossRiver(stones, D);
-//      System.out.println(isCrossRiver);
       
       if (isCrossRiver) return i;
     }
@@ -46,7 +40,6 @@ public class Solution
         int idx = (i+j < len-1) ? (i+j) : (len-1);
         isJump = (isJump || stones[idx]); 
         if (isJump) {
-//          System.out.print(idx + ", ");
           i = idx;
           break;
         }
